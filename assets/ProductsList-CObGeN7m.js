13be3280b8fdd9c205bd1c7e980e95d122fa7e35
SerhiiSemennikov <semennik@hotmail.com>
2025-06-30 10:35:37 +0200

    Updates

---
+++ assets/ProductsList-CObGeN7m.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Du6r7S0d.js";import{P as o}from"./ProductCard-D6yLazep.js";const m=({products:t,displayType:i})=>s.jsx("div",{className:"productsList",children:t.map(r=>s.jsx("div",{className:"productItem",children:s.jsx(o,{product:r,displayType:i})},r.id))});export{m as P};
